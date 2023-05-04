53c44841c4fc9db72011aa05f78e998811ec2d25
Otar Koridze <okoridze@yahoo.com>
2023-05-04 21:36:41 +0400

    Reworked to use Symfony`s DI and added parameters to use env variables.

--- src/Controller/CommissionFeeController.php
+++ src/Controller/CommissionFeeController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Service\Impl\TransactionProcessorImpl;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
